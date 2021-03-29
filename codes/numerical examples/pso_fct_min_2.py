3ad1307eac4f8cde98cb9f1da37579fc02f40231
Elvis Cui <elviscuihan@g.ucla.edu>
2021-03-29 13:36:30 +0800

    results done.

--- codes/numerical examples/pso_fct_min_2.py
+++ codes/numerical examples/pso_fct_min_2.py
@@ -2,10 +2,29 @@ import pyswarms as ps
 import matplotlib.pyplot as plt
 import numpy as np
 from matplotlib import cm
+import matplotlib
 from matplotlib.ticker import LinearLocator
 from pyswarms.utils.plotters.formatters import Mesher, Designer
 from pyswarms.utils.functions import single_obj as fx
 from pyswarms.utils.plotters import plot_cost_history, plot_contour, plot_surface
+
+#matplotlib.use("TkAgg")
+def fct_2(b):
+    """PSO test function.
+
+    Parameters
+    ----------
+    b : numpy.ndarray
+        sets of inputs shape :code:'(n_particles, 2)'
+
+    Returns
+    ----------
+    numpy.ndarray
+        computed cost of size :code:`(n_particles, )`
+    """
+    cost = 1 + b[:, 0] * np.sin(4 * np.pi * b[:, 1]) - b[:, 1] * np.sin(4 * np.pi * b[:, 0] + np.pi)
+
+    return cost
 
 def fct_3(b):
     """PSO test function: Keane function.
@@ -29,20 +48,19 @@ def fct_3(b):
 
 # Plot the sphere function's mesh for better plots
 m = Mesher(func=fct_3,
-           limits=[(-8,8), (-8,8)])
+           limits=[(-20,5), (-20,5)], levels=20, delta=0.2)
 # Adjust figure limits
-d = Designer(limits=[(-8,8), (-8,8), (-1,0)],
+d = Designer(limits=[(-20,5), (-20,5), (-1.1,0.1)],
              label=['x-axis', 'y-axis', 'z-axis'])
 
-options = {'c1':0.5, 'c2':0.3, 'w':0.9}
-optimizer = ps.single.GlobalBestPSO(n_particles=50, dimensions=2, options=options)
-optimizer.optimize(fct_3, iters=100)
+options = {'c1':1.2, 'c2':0.3, 'w':0.9}
+optimizer = ps.single.GlobalBestPSO(n_particles=25, dimensions=2, options=options, init_pos=np.random.random((25, 2))-20)
+optimizer.optimize(fct_3, iters=300)
 # Plot the cost
 #plot_cost_history(optimizer.cost_history)
 #plt.show()
-plot_contour(pos_history=optimizer.pos_history, mesher=m, designer=d, mark=(0,0))
+#plot_contour(pos_history=optimizer.pos_history, mesher=m, designer=d, mark=(0,1.39))
 print("hi")
 pos_history_3d = m.compute_history_3d(optimizer.pos_history) # preprocessing
-animation3d = plot_surface(pos_history=pos_history_3d,
-                           mesher=m, designer=d,
-                           mark=(0,0,0))
+animation3d = plot_surface(pos_history=pos_history_3d,mesher=m, designer=d,mark=(0,0,0))
+plt.show()
